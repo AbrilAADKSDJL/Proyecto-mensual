8649b95cf5692d8e1e18bb3d5c023033b76d0510
AbrilAADKSDJL <lezcanoabril896@gmail.com>
2025-12-04 18:45:19 -0300

    hola

---
+++ DataAccess/Entities/PasswordHistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class PasswordHistory
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string PasswordHash { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+    }
+}
